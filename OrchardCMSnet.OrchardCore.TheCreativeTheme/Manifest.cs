242be1f4b38a11e2e3d0c8ef75d619b71b8ba180
orchardcmsnet <blazor@orchardcms.net>
2020-01-15 09:47:49 -0700

    Added TheCreativeTheme

---
+++ OrchardCMSnet.OrchardCore.TheCreativeTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The OrchardCMS.net Creative Theme",
+    Author = "OrchardCMS.net",
+    Website = "https://OrchardCMS.net",
+    Version = "2.0.0",
+    Description = "An OrchardCMS.net theme adapted for Creative websites."
+)]
